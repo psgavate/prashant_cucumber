719f36826d6baa29ebc1cf303380aca99f2e1bc5
Prashant Gavate <pgavate@qualys.com>
2018-12-15 10:38:56 +0530

    cucumber pretty report added

--- src/test/java/Runner/RunTest.java
+++ src/test/java/Runner/RunTest.java
@@ -10,7 +10,9 @@ import cucumber.api.junit.Cucumber;
 		
 features= {"src/test/resources/features/Login_outline.feature"},
 glue= {"steps"},
-tags= {"@test1","@test2"}   //{"@tag1,@tag2"}-OR //{"@tag1","@tag2"}-and (requied both tag for same time)
+tags= {"@test1","@test2"},   //{"@tag1,@tag2"}-OR //{"@tag1","@tag2"}-and (requied both tag for same time)
+plugin= {"pretty","html:output/Report"}
+//plugin= {"com.cucmber.listner.ExtentCucumberFromatter:output/report.html"}
 		)
 
 public class RunTest {
